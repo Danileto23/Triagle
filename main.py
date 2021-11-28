9c3b1d228e967cfd8730caa7a3ca0ddc198294e8
Danileto23 <melkov_danila@bk.ru>
2021-11-28 23:45:26 +0300

    Triagle
    Triagle

---
+++ main.py
@@ -0,0 +1,22 @@
+lst = []
+for i in range(50):
+    lst.append(i+1)
+    lst.sort(reverse=True)
+
+first = lst[0]
+second = lst[1]
+s_max = 0
+polyper = 0
+stor_max = []
+
+for i in range(2, len(lst)):
+    if second + lst[i] > first and first + second > lst[i] and first + lst[i] > second:
+        polyper = (first + second + lst[i]) / 2
+        s_max = (polyper*(polyper-first)*(polyper-second)*(polyper-lst[i])) ** 0.5
+        stor_max = [first, second, lst[i]]
+
+        print("Стороны треугольника", stor_max)
+        print("max площадь ", s_max)
+    else:
+        print("Треугольник не существует")
+    break
